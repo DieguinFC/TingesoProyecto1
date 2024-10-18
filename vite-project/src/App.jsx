abe0bc527c29fa5d54a4d32057d6923ff6466f6a
DieguinFC <diego.cuevas@usach.cl>
2024-10-18 04:14:42 -0300

    added front end
    Added folder with the frontend

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>BankChat</h1> {/* Título de la página */}
+      <div className="card">
+        <p>Bancan chat?</p> {/* Mensaje de bienvenida */}
+      </div>
+    </>
+  );
+}
+
+export default App;
